8a2752ed9dae92760ceaaeedd508dfa34c83da8b
aubrey-y <aubreym.yan@gmail.com>
2020-06-08 16:02:25 -0400

    api framework

---
+++ src/main/java/xyz/gatechapi/rest/dao/Email.java
@@ -0,0 +1,14 @@
+package xyz.gatechapi.rest.dao;
+
+public class Email {
+
+    private String email;
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
